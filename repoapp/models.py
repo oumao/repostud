3f40b9a8280ca31e0926d584f4d70aec50012949
oumao <evansotis2015@gmail.com>
2019-06-25 11:24:01 +0300

    Models file is completed

--- repoapp/models.py
+++ repoapp/models.py
@@ -1,7 +1,18 @@
 from datetime import datetime
-from repoapp import db
+from repoapp import db, login_manager
+from flask_login import UserMixin
 
-class Admin(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return Admin.query.get(int(user_id))
+
+student_courses = db.Table('student_courses',
+    db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True)
+)
+
+
+class Admin(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(80), nullable=False)
@@ -23,21 +34,14 @@ class Student(db.Model):
     program = db.Column(db.Text, nullable=False)
     study_year = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    submitted_assignments = db.relationship('SubmittedAssignment', backref='student', lazy=dynamic)
+    submitted_assignments = db.relationship('SubmittedAssignment', backref='student', lazy='dynamic')
     courses = db.relationship('Course',
                             secondary=student_courses,
                             backref=db.backref('student', lazy='dynamic'),
                             lazy='dynamic')
 
     def __repr__(self):
-        return f"Student({self.fullname}, {self.admission_number},
-         {self.birth_date}, {self.program}, {self.study_year})"
-
-
-student_courses = db.Table('student_courses',
-    db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
-    db.Column('course_id', db.Integer, db.ForeignKey('course.id'), primary_key=True)
-)
+        return f"Student({self.fullname}, {self.admission_number}, {self.birth_date}, {self.program}, {self.study_year})"
 
 
 class Lecturer(db.Model):
@@ -47,7 +51,7 @@ class Lecturer(db.Model):
     email = db.Column(db.String(80), nullable=False, unique=True)
     staff_number = db.Column(db.String(40), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
-    courses = db.relationship('Course', backref='lecturer', lazy=dynamic)
+    courses = db.relationship('Course', backref='lecturer', lazy='dynamic')
 
     def __repr__(self):
         return f"Lecturer({self.fullname}, {self.email}, {self.staff_number})"
@@ -58,8 +62,8 @@ class Course(db.Model):
     course_name = db.Column(db.Text, nullable=False)
     course_code = db.Column(db.String(20), nullable=False, unique=True)
     lecturer_id = db.Column(db.Integer, db.ForeignKey('lecturer.id'), nullable=False)
-    assignments = db.relationship('Assignment', backref='course', lazy=dynamic)
-    assignments = db.relationship('SubmittedAssignment', backref='course', lazy=dynamic)
+    assignments = db.relationship('Assignment', backref='course', lazy='dynamic')
+    assignment_submitted = db.relationship('SubmittedAssignment', backref='course', lazy='dynamic')
 
     def __repr__(self):
         return f"Course({self.course_name}, {self.course_code})"
@@ -79,7 +83,7 @@ class SubmittedAssignment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     submitted_file = db.Column(db.String(40), nullable=False)
-    score = db.Column(db.Integer(20), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
     student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
     
