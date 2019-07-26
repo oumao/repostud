aba9d57513f7d0a1ed36ad828cfa2a346c84b240
oumao <evansotis2015@gmail.com>
2019-07-26 14:02:50 +0300

    Added file upload configurations

--- repoapp/__init__.py
+++ repoapp/__init__.py
@@ -2,15 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+SUBMITTED_ASSIGNMENT = "/home/ouma/Documents/python/repostud/repoapp/static/submitted_assignments"
+
 app = Flask(__name__)
-app.config['SECRET_KEY']= b'klhohoajoiK09LL81HoljUlOkhla'
+app.config['SECRET_KEY'] = b'klhohoajoiK09LL81HoljUlOkhla'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:oumaruc2016@localhost/repostud'
+app.config['SUBMITTED_ASSIGNMENT'] = SUBMITTED_ASSIGNMENT
+app.config['ALLOWED_FILE_EXTENSIONS'] = ['pdf', 'docx', 'txt', 'xlsx', 'pptx']
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_message = "You must be logged in to access this page."
 login_manager.login_view = "admin_login"
+login_manager.login_view = "student_login"
 login_manager.login_message_category = "info"
 
 from repoapp import routes
-
