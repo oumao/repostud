8d8a6333ccf797642ede505bbde7dff1be916cc6
oumao <evansotis2015@gmail.com>
2019-07-26 14:07:01 +0300

    Added Assignment upload and display routes

--- repoapp/routes.py
+++ repoapp/routes.py
@@ -1,9 +1,16 @@
-from flask import render_template, url_for, session, flash, redirect
+import os, secrets
+from flask import render_template, url_for, session, flash, redirect, request
 from flask_login import login_required, login_user, logout_user, current_user
 from repoapp import app, db
-from repoapp.forms import AdminRegistrationForm, AdminLoginForm, StudentRegistrationForm, LecturerRegistrationForm, CourseRegistrationForm
+from repoapp.forms import (AdminRegistrationForm, AdminLoginForm, AdminProfileForm,
+                        StudentRegistrationForm, StudentLoginForm,
+                        LecturerRegistrationForm, LecturerLoginForm, CourseRegistrationForm,
+                        AssignmentSubmissionForm)
 from werkzeug.security import generate_password_hash, check_password_hash
-from repoapp.models import Admin, Student, Lecturer, Course
+from werkzeug.utils import secure_filename
+from repoapp.models import Admin, Student, Lecturer, Course, SubmittedAssignment
+
+
 
 
 @app.route("/")
@@ -14,9 +21,9 @@ def home():
 
 """ Administrator Routes from Here """
 
-@app.route("/admin_registration", methods=['POST', 'GET'])
+@app.route("/admin/registration", methods=['POST', 'GET'])
 def admin_registration():
-    
+
     user_exist = Admin.query.all()
     if not user_exist:
         form = AdminRegistrationForm()
@@ -30,16 +37,12 @@ def admin_registration():
             return redirect(url_for('admin_login'))
         return render_template('admin/registration.html', title="Admin Registration", form=form)
     else:
-        flash("Administrator has already been registered into the system. \
-            Kindly login to continue!", "danger")
+        flash("Administrator has already been registered into the system. Kindly login to continue!", "danger")
         return render_template('home.html', title='Home')
 
 
-@app.route("/admin_login", methods=['POST', 'GET'])
+@app.route("/admin/login", methods=['POST', 'GET'])
 def admin_login():
-
-    if current_user.is_authenticated:
-        return redirect(url_for('admin_dashboard'))
 
     form = AdminLoginForm()
     if form.validate_on_submit():
@@ -54,6 +57,7 @@ def admin_login():
     return render_template('admin/login.html', title="Admin Login", form=form)
 
 
+
 @app.route("/logout")
 @login_required
 def logout():
@@ -65,13 +69,29 @@ def logout():
 @app.route("/admin_dashboard")
 @login_required
 def admin_dashboard():
-    return render_template('admin/dashboard.html', title="Admin")
+    head = Admin.query.all()
+    return render_template('admin/dashboard.html', title="Admin", head=head)
 
-
-@app.route("/user_profile")
+@app.route("/user_profile", methods=['GET', 'POST'])
 @login_required
 def user_profile():
-    return render_template("admin/user.html", title="Admin")
+
+    hod = Admin.query.filter_by(email=Admin.email).first()
+
+    form = AdminProfileForm()
+
+    if form.validate_on_submit():
+        hod.fullname = form.fullname.data
+        hod.email = form.email.data
+        hod.password = generate_password_hash(form.password.data)
+
+        db.session.commit()
+        flash('Your account has been successfully updated', 'success')
+        return redirect(url_for('admin_dashboard'))
+       
+    profile_pic = url_for('static', filename='assets/img/faces/' + hod.profile_pic) 
+    
+    return render_template("admin/user.html", title="Admin", form=form, profile_pic=profile_pic)
 
 
 """ Student Routes """
@@ -98,7 +118,8 @@ def add_student():
 @app.route("/student_table")
 @login_required
 def student_table():
-    return render_template("admin/student_table.html", title="Admin")
+    students = Student.query.order_by('study_year')
+    return render_template("admin/student_table.html", title="Admin", students=students)
 
 
 """ Lecturer Routes """
@@ -125,7 +146,9 @@ def add_lecturer():
 @app.route("/lecturer_table")
 @login_required
 def lecturer_table():
-    return render_template("admin/lecturer_table.html", title="Admin")
+
+    lecs = Lecturer.query.all()
+    return render_template("admin/lecturer_table.html", title="Admin", lecs=lecs)
 
 
 
@@ -137,7 +160,7 @@ def add_course():
 
     form = CourseRegistrationForm()
 
-    lect = Lecturer.query.filter_by(first_name="Nixon").first()
+    lect = Lecturer.query.filter_by(staff_number=form.staff_number.data).first()
 
     if form.validate_on_submit():
         course = Course(course_name=form.course_name.data, course_code=form.course_code.data, lecturer=lect)
@@ -148,4 +171,125 @@ def add_course():
 
     return render_template("admin/course.html", title="Admin", form=form)
 
+
+""" Logins for Student and Lecturers """
+
+@app.route('/student/login', methods=['GET', 'POST'])
+def student_login():
+    
+    if current_user.is_authenticated:
+        return redirect(url_for('student_dashboard'))
+
+    form = StudentLoginForm()
+    
+    if form.validate_on_submit():
+        student = Student.query.filter_by(admission_number=form.admission_number.data).first()
+
+        if student and check_password_hash(student.password, form.password.data):
+            login_user(student)
+            return redirect(url_for('student_dashboard'))
+        else:
+            flash(f'Invalid Admission or Password', 'danger')
+    return render_template("student/login.html", title="Student", form=form)
+
+
+#Student dashboard
+
+@app.route('/student/dashboard')
+@login_required
+def student_dashboard():
+    return render_template('student/dashboard.html', title="Student")
+
+@app.route('/student_profile')
+@login_required
+def student_profile():
+    return render_template('student/user.html', title="Student")
+
+
+#student logout 
+
+@app.route('/student/logout')
+@login_required
+def student_logout():
+    logout_user()
+    session.clear()
+    flash('Logged Out was Successfull','success')
+    return redirect(url_for('home'))
+
+
+
+
+def allowed_file(filename):
+
+    if not '.' in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.lower()  in app.config['ALLOWED_FILE_EXTENSIONS']:
+        return True
+    else: 
+        return False
+    
+
+
+@app.route('/submitted_assignment', methods=['GET', 'POST'])
+@login_required
+def submitted_assignment():
+    
+        
+    form = AssignmentSubmissionForm()
+
+    cour = Course.query.filter_by(course_code=form.course_code.data).first()
+    stud = Student.query.filter_by(admission_number=current_user.admission_number).first()
+
+    if request.method == "POST":
+
+        if request.files:
+
+            ass_file = request.files['submitted_file']
+
+            if ass_file.filename == "":
+                flash("File should have a Name", "danger")
+                return redirect(url_for('submitted_assignment'))
+
+            if not allowed_file(ass_file.filename):
+                flash("The file extension is not allowed", "danger")
+                return redirect(url_for('submitted_assignment'))
+
+            else:
+
+                filename = secure_filename(ass_file.filename)
+                ass_file.save(os.path.join(app.config['SUBMITTED_ASSIGNMENT'], filename))
+
+        if form.validate_on_submit():
+            flash("The file is Successfully Uploaded ", "success")
+            print(filename)
+
+        sub_ass = SubmittedAssignment(submitted_file=filename, score=0, student=stud, course=cour)
+        db.session.add(sub_ass)
+        db.session.commit()
+        flash('Successfully Uploaded your file', 'success')
+        return redirect(url_for('submitted_assignment', ass_file=ass_file))
+
+    
+    
+    return render_template('student/assignment.html', form=form)
+
+
+@app.route('/assignment/table', methods=['GET', 'POST'])
+def assignment_table():
+
+    ass_results = db.session.query(Course.course_code, Course.course_name, SubmittedAssignment.submitted_file).outerjoin(Course, SubmittedAssignment.course_id == Course.id).all()
+    
+    return render_template('student/assignment_table.html', title="Student", ass_results=ass_results)
+
+
+
+#Lecturer Login
+
+@app.route('/lecturer_login', methods=['GET', 'POST'])
+def lecturer_login():
+    form = LecturerLoginForm()
+    return render_template("lecturer/login.html", title="Lecturer", form=form)
 
