0a4240aa9df5a2eb80b1c68b33deaa5c6b4fac3b
ouma evans <evansotis2015@gmail.com>
2021-02-09 00:26:13 +0300

    created create_app function

--- repoapp/__init__.py
+++ repoapp/__init__.py
@@ -1,20 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 SUBMITTED_ASSIGNMENT = "/home/ouma/Documents/python/repostud/repoapp/static/submitted_assignments"
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = b'klhohoajoiK09LL81HoljUlOkhla'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Oumaruc2016@localhost/repostud'
-app.config['SUBMITTED_ASSIGNMENT'] = SUBMITTED_ASSIGNMENT
-app.config['ALLOWED_FILE_EXTENSIONS'] = ['pdf', 'docx', 'txt', 'xlsx', 'pptx']
 
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_message = "You must be logged in to access this page."
-login_manager.login_view = "admin_login"
-login_manager.login_view = "student_login"
-login_manager.login_message_category = "info"
+
+def create_app(config):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('config')
+    app.config.from_pyfile('config.py')
+
+
+
+
+    app.config['SUBMITTED_ASSIGNMENT'] = SUBMITTED_ASSIGNMENT
+    app.config['ALLOWED_FILE_EXTENSIONS'] = ['pdf', 'docx', 'txt', 'xlsx', 'pptx']
+
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
+# login_manager = LoginManager(app)
+# login_manager.login_message = "You must be logged in to access this page."
+# login_manager.login_view = "admin_login"
+# # login_manager.login_view = "student_login"
+# login_manager.login_message_category = "info"
 
 from repoapp import routes
