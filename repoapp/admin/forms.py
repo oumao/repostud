05716352fe118e63e38b65888f524303b4c31a71
ouma evans <evansotis2015@gmail.com>
2021-02-16 23:26:27 +0300

    Created Admin login form

--- repoapp/admin/forms.py
+++ repoapp/admin/forms.py
@@ -1,33 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField
-from ..models import Admin
-from flask_wtf.file import FileField, FileAllowed 
-from wtforms.fields.html5 import DateField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-
-class AdminRegistrationForm(FlaskForm):
-
-	fullname = StringField('Full Name', validators=[DataRequired(), Length(min=6, max=50)])
-	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-	submit = SubmitField('Register')
-
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from ..models import User
+from wtforms.validators import DataRequired
 
 class AdminLoginForm(FlaskForm):
 
-	email = StringField('Email', validators=[DataRequired(), Email()])
+	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember me')
 
 	submit = SubmitField('Sign In')
-
-class AdminProfileForm(FlaskForm):
-	
-	fullname = StringField('Full Name', validators=[DataRequired()])
-	email = StringField('Email', validators=[DataRequired(), Email()])
-	profile_pic = FileField('Update Profile Picture', validators=[FileAllowed('png', 'jpg')])
-	password = PasswordField('Password', validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm your Password', validators=[DataRequired(), EqualTo('password')])
-
-	submit = SubmitField('Update')
